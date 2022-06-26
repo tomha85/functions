9f69f9552352b8094ea2f334ed5fe0186d6ac4af
tomha <hhp276@gmail.com>
2022-06-26 02:31:09 +0000

    change in init http trigger

--- triggerhttp/__init__.py
+++ triggerhttp/__init__.py
@@ -3,6 +3,7 @@ import logging
 import azure.functions as func
 #fix _init_
 #put new name
+#adding branch in init
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
